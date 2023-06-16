0548543e3765083755dec92bd75bbebc80ce1c50
onovich <hjn1110@163.com>
2023-06-17 01:38:26 +0800

    <feature> add: Capability

--- Assets/com.mortise.modifier.toaster/Generic/ToasterGridTM.cs
+++ Assets/com.mortise.modifier.toaster/Generic/ToasterGridTM.cs
@@ -11,6 +11,10 @@ namespace MortiseFrame.Modifier.Toaster.Generic {
         public void SetPassable(bool[] value) => passable = value;
         public void ClearPassable() => passable = null;
 
+        [SerializeField] int[] capability;
+        public int[] Capability => capability;
+        public void SetCapability(int[] value) => capability = value;
+
         public Vector2Int CellCount;
         public Vector2 CellSize;
         public Vector2 LocalOffset;
@@ -27,6 +31,16 @@ namespace MortiseFrame.Modifier.Toaster.Generic {
             return passable[x + y * CellCount.x];
         }
 
+        public int GetCapabilityValue(Vector2Int index) {
+            var x = index.x;
+            var y = index.y;
+            var i = x + y * CellCount.x;
+            if (i >= passable.Length || i < 0) {
+                Debug.LogError($"Index out of range");
+            }
+            return capability[x + y * CellCount.x];
+        }
+
         public void SetPassableValue(Vector2Int index, bool value) {
             var x = index.x;
             var y = index.y;
@@ -35,6 +49,16 @@ namespace MortiseFrame.Modifier.Toaster.Generic {
                 Debug.LogError($"Index out of range: x = {x}, y = {y}, i = {i}, length = {passable.Length}");
             }
             passable[i] = value;
+        }
+
+        public void SetCapabilityValue(Vector2Int index, int value) {
+            var x = index.x;
+            var y = index.y;
+            var i = x + y * CellCount.x;
+            if (i >= passable.Length || i < 0) {
+                Debug.LogError($"Index out of range: x = {x}, y = {y}, i = {i}, length = {passable.Length}");
+            }
+            capability[i] = value;
         }
 
         public void Clear() {

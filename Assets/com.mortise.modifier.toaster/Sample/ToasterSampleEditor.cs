602356d7711c6d318a023bb9d557fd1308fc07f2
onovich <hongjianan@4399inc.com>
2023-06-25 11:19:03 +0800

    <refactor> modify: 重写 index2AABB 公式

--- Assets/com.mortise.modifier.toaster/Sample/ToasterSampleEditor.cs
+++ Assets/com.mortise.modifier.toaster/Sample/ToasterSampleEditor.cs
@@ -14,6 +14,7 @@ namespace MortiseFrame.Modifier.Toaster.Sample {
         [Header("每单位网格数(n x n)")] public int MPU = 1;
         [Header("本地偏移")] public Vector2 localOffse;
         public Vector2 offset => new Vector2(-UnitCount.x * 0.5f, -UnitCount.y * 0.5f) + localOffse;
+        // public Vector2 offset => localOffse;
 
         // Lock
         bool isBaked = false;
--- Assets/com.mortise.modifier.toaster/Util/BakeMathUtil.cs
+++ Assets/com.mortise.modifier.toaster/Util/BakeMathUtil.cs
@@ -8,24 +8,24 @@ namespace MortiseFrame.Modifier.Toaster.Util {
         public static AABB Index2AABB(Vector2Int index, Vector2 cellSize, Vector2 stageOffset) {
 
             var offset = cellSize / 2;
-            var x = index.x;
-            var y = index.y;
+            var center = new Vector2(index.x * cellSize.x, index.y * cellSize.y) + offset + stageOffset;
 
-            var xMin = x * cellSize.x + stageOffset.x;
-            var xMax = x * cellSize.x + offset.x * 2 + stageOffset.x;
-            var yMin = y * cellSize.y + stageOffset.y;
-            var yMax = y * cellSize.y + offset.y * 2 + stageOffset.y;
+            var xMin = center.x - cellSize.x / 2;
+            var xMax = center.x + cellSize.x / 2;
+            var yMin = center.y - cellSize.y / 2;
+            var yMax = center.y + cellSize.y / 2;
             var min = new Vector2(xMin, yMin);
             var max = new Vector2(xMax, yMax);
             var cell_aabb = new AABB(min, max);
             return cell_aabb;
-            
+
         }
 
         public static Vector2 Index2GizmosCenter(Vector2Int index, Vector2 cellSize, Vector2 stageOffset) {
 
             var cell_offset = cellSize / 2;
             var pos = new Vector2(index.x * cellSize.x, index.y * cellSize.y) + cell_offset + stageOffset;
+
             return pos;
 
         }
